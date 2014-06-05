f2f4fbbdac6191e12e475c7a8ecac57c30ad8e37
vagrant <vagrant@kellogg-dev-box>
2014-06-05 18:08:46 +0000

    First commit

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Final::Application.routes.draw do
 
 
   # Sign-In and Sign-Out
-
+  get "/logout" => 'sessions#logout'
   get "/login" => 'sessions#login'
   post "/authenticate" => 'sessions#authenticate'
 
